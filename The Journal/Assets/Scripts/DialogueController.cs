9e2b3bbdad5d981d0779e9438583638da8ec686d
Ethan Fournier <59780951+Wolfehh@users.noreply.github.com>
2022-05-27 15:18:04 -0400

    Dialogue Controller Done

--- The Journal/Assets/Scripts/DialogueController.cs
+++ The Journal/Assets/Scripts/DialogueController.cs
@@ -7,40 +7,66 @@ public class DialogueController : MonoBehaviour
 {
 
     public TextMeshProUGUI textBox;
-    public string[] sentences;
-    public float dialogueSpeed;
+    public TextAsset script;
+    private bool sentenceWrote = true;
+    private bool sentenceInterrupt = false;
+    private float dialogueSpeed = 0.05f;
+    private string[] sentences;
     private int index = 0;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        ParseDialogue();
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Return)){
-            NextSentence();
+            if (sentenceWrote)
+            {
+                sentenceInterrupt = false;
+                NextSentence();
+            }
+            else {
+                sentenceInterrupt = true;
+                StopCoroutine(WriteSentence());
+                textBox.text = sentences[index];
+                index++;
+                sentenceWrote = true;
+            }
         }
     }
 
     void NextSentence()
     {
-        if(index <= sentences.Length - 1)
+        if (index <= sentences.Length - 1)
         {
             textBox.text = "";
             StartCoroutine(WriteSentence());
         }
     }
 
+    void ParseDialogue()
+    {
+        string text = script.text;
+        sentences = text.Split('\n');
+    }
+
     IEnumerator WriteSentence()
     {
+        sentenceWrote = false;
         foreach(char letter in sentences[index].ToCharArray())
         {
+            if (sentenceInterrupt)
+            {
+                yield break;
+            }
             textBox.text += letter;
             yield return new WaitForSeconds(dialogueSpeed);
         }
         index++;
+        sentenceWrote = true;
     }
 }
